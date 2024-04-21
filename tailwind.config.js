9cf4e4b078aadf16cacd97a8a87d3979d6547feb
toshikifujiyoshi <toshikifujiyoshi1023@gmail.com>
2024-04-21 19:02:39 +0900

    ''が取れていたので修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,9 @@ module.exports = {
   ],
   theme: {
     fontSize: {
-      sm: '0.8rem',
-      base: '1rem',
-      xl: '1.25rem',
+      'sm': '0.8rem',
+      'base': '1rem',
+      'xl': '1.25rem',
       '2xl': '1.563rem',
       '3xl': '1.953rem',
       '4xl': '3rem',
