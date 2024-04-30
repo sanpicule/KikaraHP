686dd43b6fef8c8277cdd25ab352be670b8442d5
sanpicule <sannsi4444@gmail.com>
2024-04-30 20:41:24 +0900

    霊気ページの施術詳細修正

--- src/app/reiki/page.js
+++ src/app/reiki/page.js
@@ -13,6 +13,7 @@ import targetFourImage from '@/public/images/reikiTargetFour.png'
 import reikiDetailImageLeft from '@/public/images/reikiDetailLeft.png'
 import reikiDetailImageRight from '@/public/images/reikiDetailRight.png'
 import OtherServices from '@/components/features/common/OtherServices'
+import favoImage from '@/public/images/favo.png'
 
 
 const ReikiPage = () => {
@@ -95,79 +96,100 @@ const ReikiPage = () => {
           </div>
         </div>
       </div>
-      <div className='py-24 text-center max-w-[90%] mx-auto md:w-[1040px] md:tracking-wide'>
+      <div className='py-24 max-w-[90%] mx-auto md:w-[1040px] md:tracking-wide'>
         <div className="flex flex-col items-center gap-2">
           <p className='section_sub'>detail</p>
-          <h2>直傳靈氣 施術詳細</h2>
+          <h2>施術詳細</h2>
           <div className="w-12 h-0.1 bg-text-color"></div>
         </div>
         <div className='mt-12'>
-          <div className='pb-12 border-b border-slate-500'>
+          <div className='flex justify-center'>
             <p className='py-2 px-8 bg-accent-color inline-block rounded-full text-main-color'>メニュー</p>
-            <div className='grid grid-cols-2 gap-12 mt-12'>
-              <div className='border-4 border-menu-border rounded-xl px-2 py-8'>
-                <h3>●通常施術</h3>
-                <div className='mt-8'>
-                  <p className='bg-menuItem-color inline-block py-2 px-8 text-main-color rounded-full'>料金</p>
-                  <h4 className='mt-2'>3,000円/1時間</h4>
+          </div>
+          <div className='grid grid-cols-2 gap-12 mt-12'>
+            <div className='relative border-4 border-menu-border rounded-xl p-8'>
+              <Image
+                src={favoImage}
+                alt=""
+                width={100}
+                height={70}
+                className='absolute top-8 right-8'
+              />
+              <h3>通常施術</h3>
+              <div className='mt-8'>
+                <p className='bg-text-color inline-block py-2 px-8 text-main-color rounded-full'>料金</p>
+                <div className='flex flex-col gap-4 ml-8'>
+                  <h4 className='mt-2'>
+                    <span className='text-accent-color text-4xl tracking-normal'>3,000円</span>/60分(税込)
+                  </h4>
+                  <p>お申込みフォームに第一希望、第二希望をお書き下さい</p>
+                  <p>①10時～ ②13時～ ③15時～</p>
+                  <p>※当日現金払い</p>
                 </div>
               </div>
-              <div className='border-4 border-menu-border rounded-xl px-2 py-8'>
-                <h3>●遠隔施術</h3>
-                <div className='mt-8'>
-                  <p className='bg-menuItem-color inline-block py-2 px-8 text-main-color rounded-full'>料金</p>
-                  <h4 className='mt-2'>8,000円/3夜連続</h4>
-                  <p className='mt-8 bg-menuItem-color inline-block py-2 px-8 text-main-color rounded-full'>内容</p>
-                  <div className='text-start w-3/5 mx-auto'>
-                    <h4 className='mt-2'>1日目: お話しと施術</h4>
-                    <h4 className='mt-2'>2日目: 施術</h4>
-                    <h4 className='mt-2'>3日目: 施術とお伝え</h4>
+            </div>
+            <div className='border-4 border-menu-border rounded-xl p-8'>
+              <h3>遠隔施術</h3>
+              <div className='mt-8'>
+                <div>
+                  <p className='bg-text-color inline-block py-2 px-8 text-main-color rounded-full'>料金</p>
+                  <div className='flex flex-col gap-4 ml-8'>
+                    <h4>
+                      <span className='text-accent-color text-4xl tracking-normal'>8,000円</span>/3夜連続(税込)
+                    </h4>
+                    <p className='tracking-normal'>片付けをご自身でできるようになりたい方向け</p>
+                    <p className='tracking-normal'>※ 定期的にお伺いし、片付けのサポートをさせていただきます</p>
                   </div>
                 </div>
+                <div className='mt-8'>
+                  <p className='bg-text-color inline-block py-2 px-8 text-main-color rounded-full'>内容</p>
+                  <div className='flex flex-col gap-4 ml-8 mt-4'>
+                    <p className='tracking-normal'>1日目: お話しと施術</p>
+                    <p className='tracking-normal'>2日目: 施術</p>
+                    <p className='tracking-normal'>3日目: 施術とお伝え</p>
+                  </div>
+                </div>
+                <p className='mt-4'>※前納で入金確認後</p>
+                <p>※遠隔は夜のみ（21時～）</p>
               </div>
             </div>
           </div>
           <div className='mt-12'>
-            <p className='py-2 px-8 bg-accent-color inline-block rounded-full text-main-color'>場所</p>
-            <p className='mt-8'>Kikaraで行っています。アクセスはこちら</p>
-            <div className='flex mt-12 gap-4 pb-12 border-b border-slate-500'>
-              <div className='grow-none text-start'>
-                <p>〒862-0922</p>
-                <p>熊本県熊本市東区三郎1-13-38</p>
-              </div>
-              <iframe 
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2251.112437266134!2d130.75854487567454!3d32.80082585312649!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3540f1805d32dac9%3A0x8fde90f8f28c9cc6!2z44CSODYyLTA5MjIg54aK5pys55yM54aK5pys5biC5p2x5Yy65LiJ6YOO77yR5LiB55uu77yR77yT4oiS77yT77yY!5e0!3m2!1sja!2sjp!4v1713188773076!5m2!1sja!2sjp"
-                allowfullscreen
-                loading="lazy"
-                className="h-64 grow"
-                referrerpolicy="no-referrer-when-downgrade"
-              />
+            <div className='flex justify-center'>
+              <p className='py-2 px-8 bg-accent-color inline-block rounded-full text-main-color'>場所</p>
             </div>
-            <div className='mx-auto mt-12 flex flex-col gap-4 grow md:flex-row max-w-sm md:max-w-5xl'>
-              <div className='w-full'>
-                <Image
-                  src={reikiDetailImageLeft}
-                  alt=""
-                  style={{
-                    objectFit: 'cover'
-                  }}
-                />
-              </div>
-              <div className='w-full'>
-                <Image
-                  src={reikiDetailImageRight}
-                  alt=""
-                  style={{
-                    objectFit: 'cover'
-                  }}
-                />
-              </div>
+            <div className='text-center'>
+              <h4 className='mt-12'>Kikaraで行っています。熊本市東区三郎／県立大学近く</h4>
+              <p className='mt-4'>※ご予約を頂いた方には詳細な住所をお知らせします</p>
+              <p>※駐車場完備</p>
             </div>
           </div>
-        </div>
-        <button className='py-4 px-24 mt-12 bg-accent-color text-main-color transition duration-300 hover:opacity-50'>
-          <p className='text-md'>予約はこちらから</p>
-        </button>
+          <div className='mx-auto mt-12 flex flex-col gap-4 grow md:flex-row max-w-sm md:max-w-5xl'>
+            <div className='w-full'>
+              <Image
+                src={reikiDetailImageLeft}
+                alt=""
+                style={{
+                  objectFit: 'cover'
+                }}
+              />
+            </div>
+            <div className='w-full'>
+              <Image
+                src={reikiDetailImageRight}
+                alt=""
+                style={{
+                  objectFit: 'cover'
+                }}
+              />
+            </div>
+          </div>
+          <a className='flex justify-center mt-12' href='https://totonou-kikara.com/index.php?Form' target="_blank">
+            <button className='py-4 px-24 bg-accent-color text-main-color transition duration-300 hover:opacity-50'>
+              <p className='text-md'>予約はこちらから</p>
+            </button>
+          </a>
+        </div>     
       </div>
       <OtherServices />
     </div>
