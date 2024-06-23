97f6b2e29012a2b7e4f70b62ac86e2ae2ae8266a
sanpicule <sannsi4444@gmail.com>
2024-06-23 19:31:30 +0900

    問い合わせレスポンシブ対応

--- src/app/contact/confirm/page.js
+++ src/app/contact/confirm/page.js
@@ -25,25 +25,25 @@ const ConfirmPage = () => {
     <div className='mx-auto max-w-[90%] py-24 md:tracking-wide'>
       <Stepper step1={true} step2={true} />
       <div className='mx-auto mt-12 flex flex-col items-center gap-12 p-12'>
-        <div className='mx-auto flex w-[60%] items-center gap-8'>
-          <p className='w-1/3'>名前</p>
-          <p className='w-2/3'>{query.name}</p>
+        <div className='mx-auto flex w-full flex-col items-center gap-4 md:w-[60%] md:flex-row md:gap-8'>
+          <p className='w-full font-bold md:w-1/3'>名前</p>
+          <p className='ml-12 w-full md:ml-0 md:w-2/3'>{query.name}</p>
         </div>
-        <div className='mx-auto flex w-[60%] items-center gap-8'>
-          <p className='w-1/3'>フリガナ</p>
-          <p className='w-2/3'>{query.name_kana}</p>
+        <div className='mx-auto flex w-full flex-col items-center gap-4 md:w-[60%] md:flex-row md:gap-8'>
+          <p className='w-full font-bold md:w-1/3'>フリガナ</p>
+          <p className='ml-12 w-full md:ml-0 md:w-2/3'>{query.name_kana}</p>
         </div>
-        <div className='mx-auto flex w-[60%] items-center gap-8'>
-          <p className='w-1/3'>メールアドレス</p>
-          <p className='w-2/3'>{query.mail}</p>
+        <div className='mx-auto flex w-full flex-col items-center gap-4 md:w-[60%] md:flex-row md:gap-8'>
+          <p className='w-full font-bold md:w-1/3'>メールアドレス</p>
+          <p className='ml-12 w-full md:ml-0 md:w-2/3'>{query.mail}</p>
         </div>
-        <div className='mx-auto flex w-[60%] items-center gap-8'>
-          <p className='w-1/3'>メールアドレス（確認中）</p>
-          <p className='w-2/3'>{query.mail_confirm}</p>
+        <div className='mx-auto flex w-full flex-col items-center gap-4 md:w-[60%] md:flex-row md:gap-8'>
+          <p className='w-full font-bold md:w-1/3'>メールアドレス（確認用）</p>
+          <p className='ml-12 w-full md:ml-0 md:w-2/3'>{query.mail_confirm}</p>
         </div>
-        <div className='mx-auto flex w-[60%] items-center gap-8'>
-          <p className='w-1/3'>内容</p>
-          <p className='w-2/3'>{query.contents}</p>
+        <div className='mx-auto flex w-full flex-col items-center gap-4 md:w-[60%] md:flex-row md:gap-8'>
+          <p className='w-full font-bold md:w-1/3'>内容</p>
+          <p className='ml-12 w-full md:ml-0 md:w-2/3'>{query.contents}</p>
         </div>
       </div>
       <div className='text-center'>
