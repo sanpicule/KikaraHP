c0ef9b4e2d8269e30fbfb487d35bb99a40e997f2
toshikifujiyoshi <toshikifujiyoshi1023@gmail.com>
2024-09-01 15:56:36 +0900

    PCトップ看板部分のデザイン修正

--- src/components/features/home/Hero.js
+++ src/components/features/home/Hero.js
@@ -1,9 +1,8 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 import mainHeroImage from '@/public/images/mainHeroImage.png'
-import Link from 'next/link'
 import menuItemsList from '../../../data/menuItems.json'
-import arrow from '@/public/images/arrow.png'
 
 const HeroSection = () => {
   return (
@@ -21,32 +20,35 @@ const HeroSection = () => {
         <p className='text-xl'>くらし・ととのう・さろん</p>
         <h1 className='text-5xl tracking-wide'>Kikara</h1>
       </div>
-      <div className='absolute right-[100px] bottom-[100px] text-white'>
-        <p className='mt-20 text-4xl font-thin tracking-wide text-right'>
+      <div className='absolute bottom-[100px] right-[100px] text-white'>
+        <p className='mt-20 text-right text-4xl font-thin tracking-wide'>
           身体も<br></br>くらしも<br></br>食事も<br></br>ととのえる
         </p>
       </div>
-      <div className='absolute left-[100px] bottom-[100px] text-white'>
-        <h3 className='text-3xl text-left'>サービス一覧</h3>
+      <div className='absolute bottom-[100px] left-[100px] text-white'>
+        <h3 className='text-left text-3xl'>サービス一覧</h3>
         <ul className='tracking-wide'>
           {menuItemsList.map((menuItem, index) => (
-            <li key={index} className='text-xl w-[300px] ml-8 py-4'>
+            <li key={index} className='ml-8 w-[300px] py-4 text-xl'>
               <Link href={menuItem.id} className='flex items-center justify-between'>
-                <p>{menuItem.menuTitle}</p>
-                <Image src={arrow} alt='menuArrow' width={35} />
+                <p
+                  className={`before:mr-2 before:inline-block before:w-0 before:bg-kikara-white before:align-middle before:transition-all before:duration-300 before:content-[''] hover:before:h-[2px] hover:before:w-4`}
+                >
+                  {menuItem.menuTitle}
+                </p>
               </Link>
             </li>
           ))}
         </ul>
-        <div className='flex items-start mt-8'>
-          <button className='bg-secondary-brown-light px-4 py-6'>
+        <div className='mt-8 flex items-start'>
+          <button className='bg-secondary-brown-light px-4 py-6 duration-300 hover:bg-secondary-brown'>
             <Link href={'/contact'}>
-              <p className='text-kikara-white [writing-mode:vertical-rl]'>問い合わせ</p>
+              <p className='text-kikara-white [writing-mode:vertical-rl]'>お問い合わせ</p>
             </Link>
           </button>
         </div>
       </div>
-      <div className='absolute bottom-[100px] left-1/2 -translate-x-1/2 flex flex-col-reverse items-center justify-center'>
+      <div className='absolute bottom-[100px] left-1/2 flex -translate-x-1/2 flex-col-reverse items-center justify-center'>
         <span className='tracking-wide text-white'>Scroll</span>
         <div className='h-20 w-0.5 animate-bounce bg-white'></div>
       </div>
--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,24 +1,37 @@
 'use client'
 
-import Image from 'next/image'
 import Link from 'next/link'
-import React, { useState } from 'react'
-import useScrollDirection from '@/hooks/useScrollDirection'
-import arrow from '@/public/images/arrow.png'
-import menuItemsList from '../../data/menuItems.json'
-import SnsIconBtn from '../features/common/Buttons/SnsIconBtn'
 import { usePathname } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import useScrollDirection from '@/hooks/useScrollDirection'
+import menuItemsList from '../../data/menuItems.json'
 
 const Header = () => {
   const [isClick, setIsClick] = useState(false)
   const scrollDirection = useScrollDirection()
   const pathname = usePathname()
-  console.log(pathname)
+  const [hamburgerColor, setHamburgerColor] = useState('kikara-white')
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY
+      if (scrollY > 840) {
+        setHamburgerColor('secondary-brown')
+      } else {
+        setHamburgerColor('kikara-white')
+      }
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
   return (
     <div
-      className={`fixed left-0 top-0 z-50 w-screen transition-transform duration-500 ${scrollDirection === 'down' ? '-translate-y-full' : 'translate-y-0'}`}
+      className={`fixed left-0 top-0 z-50 w-screen transition-transform duration-500 ${scrollDirection === 'down' && pathname !== '/' ? '-translate-y-full' : 'translate-y-0'}`}
     >
-      <div className={`flex h-[85px] items-center px-4 md:px-12 ${pathname === '/' && 'h-auto pt-16'} ${pathname !== '/' && 'bg-primary-pink-light'}`}>
+      <div
+        className={`flex h-[85px] items-center px-4 md:px-12 ${pathname === '/' && 'h-auto pt-16'} ${pathname !== '/' && 'bg-primary-pink-light'}`}
+      >
         {pathname !== '/' && (
           <div onClick={() => setIsClick(false)}>
             <Link href={'/'}>
@@ -27,57 +40,55 @@ const Header = () => {
             </Link>
           </div>
         )}
-        <div className='ml-auto flex flex-col gap-4 z-40' onClick={() => setIsClick(!isClick)}>
-          <span
-            className={`block h-[2px] w-16 rounded-xl bg-secondary-brown ${isClick ? 'translate-y-[18px] rotate-45 transform transition duration-300' : 'transition duration-500'}`}
-          ></span>
-          <span
-            className={`block h-[2px] w-16 rounded-xl bg-secondary-brown ${isClick ? 'translate-x-full opacity-0 transition duration-300' : 'transition duration-500'}`}
-          ></span>
-          <span
-            className={`block h-[2px] w-16 rounded-xl bg-secondary-brown ${isClick ? '-translate-y-[18px] -rotate-45 transform transition duration-300' : 'transition duration-500'}`}
-          ></span>
+        <div className='group z-40 ml-auto mt-2' onClick={() => setIsClick(!isClick)}>
+          <div className='flex flex-col gap-4'>
+            <span
+              className={`block h-[2px] w-16 rounded-xl bg-${hamburgerColor} transition duration-300 ${isClick ? 'w-16 translate-y-[18px] rotate-45 transform bg-secondary-brown transition duration-300' : 'transition duration-500'} ${pathname !== '/' && 'bg-secondary-brown'}`}
+            ></span>
+            <span
+              className={`block h-[2px] w-16 rounded-xl bg-${hamburgerColor} transition-all duration-300 group-hover:w-12 ${isClick ? '!w-16 translate-x-full bg-secondary-brown opacity-0 transition duration-300' : 'transition duration-500'} ${pathname !== '/' && 'bg-secondary-brown'}`}
+            ></span>
+            <span
+              className={`rounded-x block h-[2px] w-16 bg-${hamburgerColor} transition-all duration-300 group-hover:w-8 ${isClick ? '!w-16 -translate-y-[18px] -rotate-45 transform bg-secondary-brown transition duration-300' : 'transition duration-500'} ${pathname !== '/' && 'bg-secondary-brown'}`}
+            ></span>
+          </div>
+          <p
+            className={`mt-2 text-center text-sm text-${hamburgerColor} duration-300 ${isClick ? 'text-secondary-brown transition-all duration-300' : 'transition duration-500'} ${pathname !== '/' && 'text-secondary-brown'}`}
+          >
+            MENU
+          </p>
         </div>
       </div>
       {/* ハンバーガーメニュー */}
       <div
-        className={`absolute top-0 left-full h-screen w-screen bg-white px-4 pt-[85px] pb-4 transition duration-500 ${isClick && '-translate-x-full'}`}
+        className={`absolute left-full top-0 h-screen w-screen bg-white px-4 pb-4 pt-[85px] transition duration-500 ${isClick && '-translate-x-full'} ${pathname === '/' && '!pt-[150px]'}`}
       >
         <div className='mx-auto w-[90%]'>
-          <h4 className='text-secondary-brown-light'>サービス</h4>
-          <div className='pl-2'>
-            <ul className='ml-auto mr-10 w-full tracking-wide'>
+          <div className='flex flex-col items-center justify-center'>
+            <p className='text-md'>くらし・ととのう・さろん</p>
+            <p className='text-[4rem]'>Kikara</p>
+          </div>
+          <div className='mt-20'>
+            <ul className='w-full tracking-wide'>
               {menuItemsList.map((menuItem, index) => (
-                <li key={index} className='border-b border-dashed py-4' onClick={() => setIsClick(false)}>
-                  <Link href={menuItem.url} className='flex items-center justify-between'>
-                    <p>{menuItem.menuTitle}</p>
-                    <Image src={arrow} alt='menuArrow' width={35} />
+                <li key={index} className='mt-6 py-4 text-center' onClick={() => setIsClick(false)}>
+                  <Link href={menuItem.url}>
+                    <p
+                      className={`text-xl before:mr-2 before:inline-block before:w-0 before:bg-secondary-brown before:align-middle before:transition-all before:duration-300 before:content-[''] hover:before:h-[1px] hover:before:w-4`}
+                    >
+                      {menuItem.menuTitle}
+                    </p>
                   </Link>
                 </li>
               ))}
             </ul>
-          </div>
-          <h4 className='mt-8 text-secondary-brown-light'>予約</h4>
-          <div className='pl-2'>
-            <ul className='ml-auto mr-10 w-full tracking-wide'>
-              {menuItemsList.map((menuItem, index) => (
-                <li key={index} className='flex items-center justify-between py-4'>
-                  <p>{menuItem.menuTitle}</p>
-                  {menuItem.url === '/mineral' ? (
-                    <div className='flex gap-4'>
-                      <SnsIconBtn snsId={'instagram'} size={40} />
-                      <SnsIconBtn snsId={'facebook'} size={40} />
-                    </div>
-                  ) : (
-                    <button className='bg-secondary-brown-light px-4 py-1'>
-                      <a href={menuItem.reserveUrl} target='_blank'>
-                        <p className='text-kikara-white'>申込へ</p>
-                      </a>
-                    </button>
-                  )}
-                </li>
-              ))}
-            </ul>
+            <div className='mt-24 flex items-center justify-center'>
+              <button className='rounded-full bg-secondary-brown-light px-4 py-6 duration-300 hover:bg-secondary-brown'>
+                <Link href={'/contact'}>
+                  <p className='text-kikara-white'>お問い合わせ</p>
+                </Link>
+              </button>
+            </div>
           </div>
         </div>
       </div>
