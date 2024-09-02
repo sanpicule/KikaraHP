fd0749396772cc8916429be76c685528ee9e898e
toshikifujiyoshi <toshikifujiyoshi1023@gmail.com>
2024-09-02 23:31:32 +0900

    ハンバーガーメニュー内のUI改修

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,15 +1,22 @@
 'use client'
 
+import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { useEffect, useState } from 'react'
+import menuItemsList from '@/data/menuItems.json'
 import useScrollDirection from '@/hooks/useScrollDirection'
-import menuItemsList from '../../data/menuItems.json'
+import serviceMineral from '@/public/images/serviceMineral.png'
+import serviceReiki from '@/public/images/serviceReiki.png'
+import serviceTidying from '@/public/images/serviceTidying.png'
+import './style.css'
 
 const Header = () => {
   const [isClick, setIsClick] = useState(false)
-  const scrollDirection = useScrollDirection()
   const pathname = usePathname()
+
+  // ↓スクロール位置によってハンバーガーメニューの色を変更する
+  const scrollDirection = useScrollDirection()
   const [hamburgerColor, setHamburgerColor] = useState('kikara-white')
   useEffect(() => {
     const handleScroll = () => {
@@ -25,6 +32,24 @@ const Header = () => {
       window.removeEventListener('scroll', handleScroll)
     }
   }, [])
+
+  // ハンバーガーメニュー内のホバーされたリストに応じて画像を変更する
+  const [displayedImage, setdisplayedImage] = useState(serviceMineral)
+  const judgeMenuList = (menu) => {
+    switch (menu) {
+      case '/mineral':
+        setdisplayedImage(serviceMineral)
+        break
+      case '/tidying':
+        setdisplayedImage(serviceTidying)
+        break
+      case '/reiki':
+        setdisplayedImage(serviceReiki)
+        break
+      default:
+        setdisplayedImage(serviceMineral)
+    }
+  }
   return (
     <div
       className={`fixed left-0 top-0 z-50 w-screen transition-transform duration-500 ${scrollDirection === 'down' && pathname !== '/' ? '-translate-y-full' : 'translate-y-0'}`}
@@ -61,33 +86,48 @@ const Header = () => {
       </div>
       {/* ハンバーガーメニュー */}
       <div
-        className={`absolute left-full top-0 h-screen w-screen bg-white px-4 pb-4 pt-[85px] transition duration-500 ${isClick && '-translate-x-full'} ${pathname === '/' && '!pt-[150px]'}`}
+        className={`absolute left-full top-0 h-screen w-screen bg-white  transition duration-500 ${isClick && '-translate-x-full'}`}
       >
-        <div className='mx-auto w-[90%]'>
-          <div className='flex flex-col items-center justify-center'>
-            <p className='text-md'>くらし・ととのう・さろん</p>
-            <p className='text-[4rem]'>Kikara</p>
+        <div className='mx-auto flex justify-center'>
+          <div className='w-1/2'>
+            <Image
+              src={displayedImage}
+              alt=''
+              width={'full'}
+              height={'100vh'}
+              className='h-screen w-auto object-cover'
+            />
           </div>
-          <div className='mt-20'>
-            <ul className='w-full tracking-wide'>
-              {menuItemsList.map((menuItem, index) => (
-                <li key={index} className='mt-6 py-4 text-center' onClick={() => setIsClick(false)}>
-                  <Link href={menuItem.url}>
-                    <p
-                      className={`text-xl before:mr-2 before:inline-block before:w-0 before:bg-secondary-brown before:align-middle before:transition-all before:duration-300 before:content-[''] hover:before:h-[1px] hover:before:w-4`}
+          <div className={`w-1/2 px-4 pb-4 pt-[150px]`}>
+            <Link href={'/'} className='flex flex-col items-center justify-center' onClick={() => setIsClick(false)}>
+              <p className='text-md'>くらし・ととのう・さろん</p>
+              <p className='text-[4rem]'>Kikara</p>
+            </Link>
+            <div className='mt-20'>
+              <ul className='list-container w-full tracking-wide'>
+                {menuItemsList.map((menuItem, index) => (
+                  <li
+                    key={index}
+                    className={`list-content mt-6 flex h-20 items-center justify-center text-center text-xl`}
+                    onClick={() => setIsClick(false)}
+                    onMouseEnter={() => judgeMenuList(menuItem.url)}
+                  >
+                    <Link
+                      href={menuItem.url}
+                      className={`flex h-full w-full items-center justify-center before:mr-2 before:inline-block before:w-0 before:bg-secondary-brown before:align-middle before:transition-all before:duration-300 before:content-[''] hover:before:h-[1px] hover:before:w-4`}
                     >
                       {menuItem.menuTitle}
-                    </p>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+              <div className='mt-24 flex items-center justify-center'>
+                <button className='rounded-full bg-secondary-brown-light px-4 py-6 duration-300 hover:bg-secondary-brown'>
+                  <Link href={'/contact'}>
+                    <p className='text-kikara-white'>お問い合わせ</p>
                   </Link>
-                </li>
-              ))}
-            </ul>
-            <div className='mt-24 flex items-center justify-center'>
-              <button className='rounded-full bg-secondary-brown-light px-4 py-6 duration-300 hover:bg-secondary-brown'>
-                <Link href={'/contact'}>
-                  <p className='text-kikara-white'>お問い合わせ</p>
-                </Link>
-              </button>
+                </button>
+              </div>
             </div>
           </div>
         </div>
