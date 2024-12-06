270727c41e4fab9d4fea1e0df4e73ff54fb0c23f
toshikifujiyoshi <toshikifujiyoshi1023@gmail.com>
2024-12-06 22:25:51 +0900

    🩹 ホームページのアニメーションがpage.jsないではなくheroコンポーネント内にあったため、移動した

--- src/components/features/home/Hero.js
+++ src/components/features/home/Hero.js
@@ -6,54 +6,15 @@ import Link from 'next/link'
 import { Link as ScrollLink } from 'react-scroll'
 import menuItemsList from '@/data/menuItems.json'
 import useAnimate from '@/hooks/useAnimation'
-import initImage from '@/public/images/initImage.png'
 import mainHeroImage from '@/public/images/mainHeroImage.png'
 import useStore from '@/store/store'
 
 const HeroSection = () => {
-  const { isDisplay, setIsHidden, setIsDisplay } = useStore()
+  const { isDisplay } = useStore()
   const animate = useAnimate()
 
   return (
     <div className='relative w-full'>
-      <motion.div
-        className={`absolute left-0 top-0 z-40 h-svh w-full`}
-        animate={{
-          opacity: [1, 0.75, 0],
-          transitionEnd: {
-            display: 'none',
-          },
-        }}
-        transition={{
-          duration: 1.5,
-          times: [0, 0.75, 1],
-        }}
-        onAnimationComplete={() => {
-          setIsHidden(true), setIsDisplay(true)
-        }}
-      >
-        <Image
-          src={initImage}
-          alt='hero_image'
-          width={0}
-          height={0}
-          className='relative h-full w-full'
-          style={{
-            objectFit: 'cover',
-          }}
-        />
-        <motion.div
-          className='absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center'
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{
-            duration: 1.5,
-          }}
-        >
-          <p className='text-xs md:text-md'>くらし・ととのう・さろん</p>
-          <p className='text-4xl tracking-wide md:text-[4rem]'>Kikara</p>
-        </motion.div>
-      </motion.div>
       <Image
         src={mainHeroImage}
         alt='hero_image'
