9f72e8b9bebf7dfd37ced352a75f810248f0c573
toshikifujiyoshi <toshikifujiyoshi1023@gmail.com>
2025-02-28 16:12:22 +0900

    🔥 不要なコードの削除、usememoにてパフォーマンス向上

--- src/components/layouts/Body.js
+++ src/components/layouts/Body.js
@@ -2,7 +2,7 @@
 
 import { AnimatePresence } from 'framer-motion'
 import { usePathname } from 'next/navigation'
-import useStore from '@/store/store'
+import { useMemo } from 'react'
 import '@/app/globals.css'
 import Footer from './Footer'
 import Header from './Header'
@@ -10,25 +10,18 @@ import PageAnimation from './PageAnimation'
 
 const Body = ({ children }) => {
   const pathname = usePathname()
-  const { setIsAnimating, setIsAnimationCompleted, setIsHomeAnimating, setIsHomeAnimationCompleted } = useStore()
+
   // アニメーションを適用しないページのリスト
-  const noAnimationPages = ['/contact', '/contact/confirm', '/contact/complete']
+  const noAnimationPages = useMemo(() => ['/contact', '/contact/confirm', '/contact/complete'], [])
+
   // アニメーションを適用するかどうかの判定
-  const shouldAnimate = !noAnimationPages.includes(pathname)
-  const isTopPage = pathname === '/'
+  const shouldAnimate = useMemo(() => !noAnimationPages.includes(pathname), [pathname, noAnimationPages])
+  const isTopPage = useMemo(() => pathname === '/', [pathname])
 
   return (
     <AnimatePresence mode='wait'>
       <body key={pathname} className='relative font-sans'>
-        {shouldAnimate && (
-          <PageAnimation
-            isTopPage={isTopPage}
-            setIsAnimating={setIsAnimating}
-            setIsAnimationCompleted={setIsAnimationCompleted}
-            setIsHomeAnimating={setIsHomeAnimating}
-            setIsHomeAnimationCompleted={setIsHomeAnimationCompleted}
-          />
-        )}
+        {shouldAnimate && <PageAnimation isTopPage={isTopPage} />}
         <Header />
         <main className='min-h-[calc(100vh-465px)] overflow-x-hidden bg-kikara-white'>{children}</main>
         <Footer />
--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -10,10 +10,8 @@ import serviceMineral from '@/public/images/serviceMineral.png'
 import serviceReiki from '@/public/images/serviceReiki.png'
 import serviceTidying from '@/public/images/serviceTidying.png'
 import './style.css'
-import useStore from '@/store/store'
 
 const Header = () => {
-  const { isHomeAnimating, isHomeAnimationCompleted, setIsHomeAnimating, setIsHomeAnimationCompleted } = useStore()
   const [isClick, setIsClick] = useState(false)
   const [isImageChanging, setIsImageChanging] = useState(false)
   const pathname = usePathname()
@@ -21,17 +19,6 @@ const Header = () => {
   const scrollDirection = useScrollDirection()
   const [hamburgerColor, setHamburgerColor] = useState('kikara-white')
   useEffect(() => {
-    if (pathname === '/') {
-      setIsHomeAnimating(false)
-      setTimeout(() => {
-        setIsHomeAnimating(true)
-      }, 2000)
-    } else {
-      setTimeout(() => {
-        setIsHomeAnimating(true)
-        setIsHomeAnimationCompleted(true)
-      }, 800)
-    }
     const handleScroll = () => {
       const scrollY = window.scrollY
       if (scrollY > 840) {
@@ -44,7 +31,7 @@ const Header = () => {
     return () => {
       window.removeEventListener('scroll', handleScroll)
     }
-  }, [pathname, setIsHomeAnimating, setIsHomeAnimationCompleted])
+  }, [pathname])
 
   // ハンバーガーメニュー内のホバーされたリストに応じて画像を変更する
   const [displayedImage, setDisplayedImage] = useState(serviceMineral)
@@ -91,32 +78,30 @@ const Header = () => {
             </Link>
           </div>
         )}
-        {isHomeAnimating && isHomeAnimationCompleted && (
-          <div
-            className={`group z-30 ml-auto mt-2`}
-            onClick={() => {
-              setIsClick(!isClick)
-              setCurrentMenu('')
-            }}
-          >
-            <div className='flex flex-col gap-4'>
-              <span
-                className={`block h-[2px] w-16 rounded-xl bg-${hamburgerColor} transition duration-300 ${isClick ? 'w-16 translate-y-[18px] rotate-45 transform bg-secondary-brown transition duration-300' : 'transition duration-500'} ${pathname !== '/' && 'bg-secondary-brown'}`}
-              ></span>
-              <span
-                className={`block h-[2px] w-16 rounded-xl bg-${hamburgerColor} transition-all duration-300 group-hover:w-12 ${isClick ? '!w-16 translate-x-full bg-secondary-brown opacity-0 transition duration-300' : 'transition duration-500'} ${pathname !== '/' && 'bg-secondary-brown'}`}
-              ></span>
-              <span
-                className={`rounded-x block h-[2px] w-16 bg-${hamburgerColor} transition-all duration-300 group-hover:w-8 ${isClick ? '!w-16 -translate-y-[18px] -rotate-45 transform bg-secondary-brown transition duration-300' : 'transition duration-500'} ${pathname !== '/' && 'bg-secondary-brown'}`}
-              ></span>
-            </div>
-            <p
-              className={`mt-2 text-center text-sm text-${hamburgerColor} duration-300 ${isClick ? 'text-secondary-brown transition-all duration-300' : 'transition duration-500'} ${pathname !== '/' && 'text-secondary-brown'}`}
-            >
-              MENU
-            </p>
+        <div
+          className={`group z-30 ml-auto mt-2`}
+          onClick={() => {
+            setIsClick(!isClick)
+            setCurrentMenu('')
+          }}
+        >
+          <div className='flex flex-col gap-4'>
+            <span
+              className={`block h-[2px] w-16 rounded-xl bg-${hamburgerColor} transition duration-300 ${isClick ? 'w-16 translate-y-[18px] rotate-45 transform bg-secondary-brown transition duration-300' : 'transition duration-500'} ${pathname !== '/' && 'bg-secondary-brown'}`}
+            ></span>
+            <span
+              className={`block h-[2px] w-16 rounded-xl bg-${hamburgerColor} transition-all duration-300 group-hover:w-12 ${isClick ? '!w-16 translate-x-full bg-secondary-brown opacity-0 transition duration-300' : 'transition duration-500'} ${pathname !== '/' && 'bg-secondary-brown'}`}
+            ></span>
+            <span
+              className={`rounded-x block h-[2px] w-16 bg-${hamburgerColor} transition-all duration-300 group-hover:w-8 ${isClick ? '!w-16 -translate-y-[18px] -rotate-45 transform bg-secondary-brown transition duration-300' : 'transition duration-500'} ${pathname !== '/' && 'bg-secondary-brown'}`}
+            ></span>
           </div>
-        )}
+          <p
+            className={`mt-2 text-center text-sm text-${hamburgerColor} duration-300 ${isClick ? 'text-secondary-brown transition-all duration-300' : 'transition duration-500'} ${pathname !== '/' && 'text-secondary-brown'}`}
+          >
+            MENU
+          </p>
+        </div>
       </div>
       {/* ハンバーガーメニュー */}
       <div
